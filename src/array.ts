06760ce4a06fdacf7ddd9ae8055ec3ed3a5c6698
Masaru Masaru <7475621+oz001@users.noreply.github.com>
2021-08-12 13:16:24 +0900

    add array.ts

---
+++ src/array.ts
@@ -0,0 +1,20 @@
+export { };
+let numbers: number[] = [1, 2, 3, 4];
+console.log({ array: numbers });
+
+let numbers2: Array<number> = [1, 2, 3];
+console.log({ array: numbers2 });
+
+let strings2: Array<string> = ['tokyo', 'kyoto', 'osaka'];
+
+let strings: string[] = ['TypeString', 'Javascript', 'CoffeeScript'];
+
+let two_dimension: number[][] = [
+  [10, 20],
+  [30, 50],
+];
+
+let gene_array: (string | number | boolean)[] = [1, true, 'japan'];
+
+console.log(gene_array);
+
